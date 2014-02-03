e4514acf523605542dca5e96b8ee03342ad551f4
RCu <roberto.cuervo@metromec.ch>
2014-02-03 11:41:22 +0100

    First Commit

---
+++ src/backupTool/PrinterMain.java
@@ -0,0 +1,21 @@
+package backupTool;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PrinterMain {
+
+	public static void main(String[] args) {
+
+		PrintFiles pf = new PrintFiles();
+		Path source = Paths.get(args[0]);
+		try {
+			Files.walkFileTree(source, pf);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
